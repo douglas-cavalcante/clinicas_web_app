ef61a01a63108e8e58a83d8a4e0312a7d0534a2f
Douglas Cavalcante <henriquedouglas2803@gmail.com>
2019-12-28 00:41:07 -0300

    Adiciona textarea

---
+++ src/components/Form/Textarea/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+export default function Textarea({
+  col,
+  label,
+  value,
+  name,
+  onChange,
+  id,
+
+  placeholder,
+}) {
+  return (
+    <div className={`col-md-${col}`}>
+      <div className="form-group">
+        {label && <label htmlFor={id}>{label}</label>}
+        <textarea
+          className="form-control"
+          id={id}
+          name={name}
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+          rows="3"
+        />
+      </div>
+    </div>
+  );
+}
+
+Textarea.propTypes = {
+  col: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+};
